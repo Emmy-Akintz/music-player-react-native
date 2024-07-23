040502b26ffda754a03b81a8a6b068aaa495d068
Ayomide Akintan <emzyakints2005@gmail.com>
2024-07-23 15:00:25 +0100

    working on some default styles

--- music-player/src/app/_layout.tsx
+++ music-player/src/app/_layout.tsx
@@ -5,12 +5,11 @@ export default function App() {
 	return (
 		<View style={styles.container}>
 			<Text>Open up App.tsx to start working on your app!</Text>
-			<Text style={styles.play}>This is my first react-native app!</Text>
 			<StatusBar style="auto" />
 		</View>
 	)
 }
-//! tutorial 3 next
+
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
@@ -18,7 +17,4 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 		justifyContent: 'center',
 	},
-	play: {
-		color: 'blue'
-	}
 })

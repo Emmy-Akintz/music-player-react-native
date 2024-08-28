9bd7e8266f38e7d0bf4c55dafb6676ef213e25ca
Ayomide Akintan <emzyakints2005@gmail.com>
2024-08-28 11:06:27 +0100

    worked on app style (dark mode)

--- music-player/src/app/(tabs)/_layout.tsx
+++ music-player/src/app/(tabs)/_layout.tsx
@@ -2,6 +2,7 @@ import { colors, fontSize } from "@/constants/tokens"
 import { BlurView } from "expo-blur"
 import { Tabs } from "expo-router"
 import { StyleSheet } from "react-native"
+import { FontAwesome, FontAwesome6, Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
 
 const TabsNavigation = () => {
     return (
@@ -28,10 +29,22 @@ const TabsNavigation = () => {
                 }}
             />,
         }}>
-            <Tabs.Screen name="favorites" />
-            <Tabs.Screen name="playlists" />
-            <Tabs.Screen name="(songs)" />
-            <Tabs.Screen name="artists" />
+            <Tabs.Screen name="favorites" options={{
+                title: "Favourites",
+                tabBarIcon: ({ color }) => <FontAwesome name="heart" size={20} color={color} />
+            }} />
+            <Tabs.Screen name="playlists" options={{
+                title: "Playlists",
+                tabBarIcon: ({ color }) => <MaterialCommunityIcons name="playlist-play" size={28} color={color} />
+            }} />
+            <Tabs.Screen name="(songs)" options={{
+                title: "Songs",
+                tabBarIcon: ({ color }) => <Ionicons name="musical-notes-sharp" size={24} color={color} />
+            }} />
+            <Tabs.Screen name="artists" options={{
+                title: "Artists",
+                tabBarIcon: ({ color }) => <FontAwesome6 name="users-line" size={20} color={color} />
+            }} />
         </Tabs>
     )
 }
